6ae341c58ccb71c34104c0a95903c200da276d8d
GibboK <gibbok@users.noreply.github.com>
2020-02-14 12:57:25 +0100

    Setup project (#1)
    Setup project includes: TypeScript, StoryBook, NPM and NVM

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/**/*.stories.ts'],
+  addons: [
+    '@storybook/preset-typescript',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register'
+  ]
+};
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve("awesome-typescript-loader")
+      },
+    ]
+  });
+  config.resolve.extensions.push(".ts", ".tsx");
+  return config;
+};
