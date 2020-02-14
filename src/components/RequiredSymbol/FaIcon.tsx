8123741e2367517a4a5722945264cd889c6dfe0f
GibboK <gibbok.coding@gmail.com>
2020-02-14 14:10:01 +0100

    Remove components

--- src/components/RequiredSymbol/FaIcon.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-type Props = Readonly<{
-  icon: string;
-  id?: string;
-}>;
-
-export const FaIcon = ({ icon, id }: Props) => <i className={`fa fa-${icon}`} id={id} />;
