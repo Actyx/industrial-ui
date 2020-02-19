78b1235c27dd201e81022c69093d5b02f01924cb
GibboK <gibbok.coding@gmail.com>
2020-02-19 17:07:55 +0100

    Cleanup stories

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -8,8 +8,25 @@ import { TableBody } from './TableBody';
 import { TableCell } from './TableCell';
 import { TableHeader } from './TableHeader';
 import { TableRow } from './TableRow';
+import { action } from '@storybook/addon-actions';
 
 const COLOR_WHITE = theme.palette.grey.white;
+const content = (
+  <>
+    <TableCell>
+      <Typography variant="standard">Machine Cleaning: ABC 1 of 20</Typography>
+    </TableCell>
+    <TableCell>
+      <Typography variant="standard">Johanna Maltese</Typography>
+    </TableCell>
+    <TableCell>
+      <Typography variant="standard">Finished</Typography>
+    </TableCell>
+    <TableCell numeric>
+      <Typography variant="standard">10</Typography>
+    </TableCell>
+  </>
+);
 
 const createTableCell = (
   children: React.ReactNode,
@@ -55,20 +72,114 @@ storiesOf('Components|Table.Table', module)
       backgroundColor: theme.palette.grey.dark400
     })
   )
-  .add('body', () => (
+  .add('Body', () => (
     <Table>
       <TableBody>{createRows(dataBody)}</TableBody>
     </Table>
   ))
-  .add('header body', () => (
+  .add('Header body', () => (
     <Table alternateColor>
       <TableHeader>{createRows(dataHeader, 'subtext')}</TableHeader>
       <TableBody>{createRows(dataBody)}</TableBody>
     </Table>
   ))
-  .add('header body alternateColor dark', () => (
+  .add('Header body alternateColor dark', () => (
     <Table alternateColor dark>
       <TableHeader>{createRows(dataHeader, 'subtext', true)}</TableHeader>
       <TableBody>{createRows(dataBody, undefined, true)}</TableBody>
+    </Table>
+  ))
+  .add('Part: TableBody base', () => (
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            <Typography variant="standard">Machine Cleaning: ABC 1 of 20</Typography>
+          </TableCell>
+          <TableCell>
+            <Typography variant="standard">Johanna Maltese</Typography>
+          </TableCell>
+          <TableCell>
+            <Typography variant="standard">Finished</Typography>
+          </TableCell>
+          <TableCell numeric>
+            <Typography variant="standard">10</Typography>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  ))
+  .add('Part: TableCell base', () => (
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            <Typography variant="standard">10</Typography>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  ))
+  .add('Part: TableCell numeric', () => (
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell numeric>
+            <Typography variant="standard">10</Typography>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  ))
+  .add('Part: TableCell cellType', () => (
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell numeric cellType={'cellType'}>
+            <Typography variant="standard">10</Typography>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  ))
+  .add('Part TableHeader base', () => (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableCell>
+            <Typography variant="subtext">Description</Typography>
+          </TableCell>
+          <TableCell>
+            <Typography variant="subtext">User</Typography>
+          </TableCell>
+          <TableCell>
+            <Typography variant="subtext">Status</Typography>
+          </TableCell>
+          <TableCell numeric>
+            <Typography variant="subtext">Amount</Typography>
+          </TableCell>
+        </TableRow>
+      </TableHeader>
+    </Table>
+  ))
+  .add('Part: TableRow base', () => (
+    <Table>
+      <TableBody>
+        <TableRow>{content}</TableRow>
+      </TableBody>
+    </Table>
+  ))
+  .add('Part: TableRow short', () => (
+    <Table>
+      <TableBody>
+        <TableRow condensed>{content}</TableRow>
+      </TableBody>
+    </Table>
+  ))
+  .add('Part: TableRow onSelect', () => (
+    <Table>
+      <TableBody>
+        <TableRow onSelect={action('onSelect')}>{content}</TableRow>
+      </TableBody>
     </Table>
   ));
