ca23aa90e5ab8ba6fe1ef2ab05b8f42d2cd04953
GibboK <gibbok.coding@gmail.com>
2020-02-20 11:12:34 +0100

    Revise TooltipGuide

--- src/components/TooltipGuide/TooltipGuide.stories.tsx
+++ src/components/TooltipGuide/TooltipGuide.stories.tsx
@@ -1,10 +1,8 @@
 import { storiesOf } from '@storybook/react';
-import { hostDecorator } from '../../utils';
+import { hostDecorator, LoremIpsumShort } from '../../utils';
 import * as React from 'react';
 import { TooltipGuide } from './TooltipGuide';
 
-const content = 'Enter an estimated number of items to be cleaned for this activity.';
-
 storiesOf('Components|TooltipGuide', module)
   .addDecorator(hostDecorator({}))
-  .add('Base', () => <TooltipGuide color="neutral" content={content} />);
+  .add('Base', () => <TooltipGuide content={LoremIpsumShort} />);
