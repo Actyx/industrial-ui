fc3ac41891096f8817a394456ba4e108e910dfd2
GibboK <gibbok.coding@gmail.com>
2020-02-21 10:57:53 +0100

    Revise API NumericKeypad

--- src/components/NumericKeypad/NumericKeypad.tsx
+++ src/components/NumericKeypad/NumericKeypad.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ClearableInput } from '../ClearableInput';
-import { DECIMAL_IDENTIFIER, Keypad, MINUS_IDENTIFIER } from './Keypad';
+import { Keypad } from './Keypad';
 
 type Props = Readonly<{
   className?: string;
@@ -11,6 +11,9 @@ type Props = Readonly<{
   hideNegative?: boolean;
   onChange: (value: number) => void;
 }>;
+
+export const DECIMAL_IDENTIFIER = '.';
+export const MINUS_IDENTIFIER = '-';
 
 const handleSubsequentSeparator = (separator: string) => (currentValue: string, newValue: string) =>
   currentValue.includes(separator) && newValue.includes(separator)
