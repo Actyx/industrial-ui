82d902b6a76bfc14485247597b95ebc148d85ef5
GibboK <gibbok.coding@gmail.com>
2020-02-24 13:46:52 +0100

    Revise CheckBox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,18 +1,40 @@
+import { theme } from '../../theme';
 import * as React from 'react';
-import { CheckboxIndeterminate, CheckboxIndeterminateColor } from '../CheckboxIndeterminate';
+import { MUIcon } from '../MUIcon';
+import { TouchRipple } from '../TouchRipple';
+
+export type CheckboxColor = 'neutral' | 'primary';
+
+export type CheckboxState = 'indeterminate' | 'checked' | 'unchecked';
 
 type Props = Readonly<{
-  checked: boolean;
-  color?: CheckboxIndeterminateColor;
+  state: CheckboxState;
+  color?: CheckboxColor;
   disabled?: boolean;
-  onChange: () => void;
+  onClick?: () => void;
 }>;
 
-export const Checkbox = ({ checked, color, disabled, onChange }: Props) => (
-  <CheckboxIndeterminate
-    state={checked ? 'checked' : 'unchecked'}
-    color={color}
-    disabled={disabled}
-    onClick={onChange}
-  />
-);
+export const Checkbox = ({ state, color, disabled, onClick }: Props) => {
+  const iconType =
+    state === 'checked'
+      ? 'check_box'
+      : state === 'unchecked'
+      ? 'check_box_outline_blank'
+      : 'indeterminate_check_box';
+
+  return (
+    <div
+      style={{
+        color: theme.utils.rgba(
+          color === 'neutral' ? theme.palette.grey[900] : theme.palette.primary.main,
+          disabled ? 0.5 : 1
+        )
+      }}
+      onClick={onClick}
+    >
+      <TouchRipple>
+        <MUIcon fontSize={40} type={iconType} />
+      </TouchRipple>
+    </div>
+  );
+};
