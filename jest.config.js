f8feb5e9ee72dd05213a5b7e820a1dedafa5d80b
GibboK <gibbok.coding@gmail.com>
2020-02-24 16:24:30 +0100

    Add Jest test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,17 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy'
   },
-  setupFiles: ['<rootDir>./jest.setup.js']
+  setupFiles: ['<rootDir>./jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.{stories.tsx}', '!**/*.d.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageThreshold: {
+    global: {
+      statements: 13,
+      branches: 14,
+      lines: 13,
+      functions: 6
+    }
+  }
 };
