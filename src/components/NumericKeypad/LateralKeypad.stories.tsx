2b6c27f9960adbe7fbd1289de9fc288875641279
GibboK <gibbok.coding@gmail.com>
2020-02-24 11:41:57 +0100

    Add story for LateralKeypad with animation

--- src/components/NumericKeypad/LateralKeypad.stories.tsx
+++ src/components/NumericKeypad/LateralKeypad.stories.tsx
@@ -4,6 +4,21 @@ import { hostDecorator } from '../../utils';
 import * as React from 'react';
 import { LateralKeypad } from './LateralKeypad';
 
+const ShowAnimation = ({}) => {
+  const [open, setOpen] = React.useState<boolean>(true);
+  return (
+    <>
+      Open/Close with an animation:
+      <input type="checkbox" checked={open} onChange={() => setOpen(!open)}></input>
+      <LateralKeypad
+        open={open}
+        onChange={action('onChange')}
+        onDeleteSelect={action('onDeleteSelect')}
+      />
+    </>
+  );
+};
+
 storiesOf('Components|NumericKeypad.LateralKeypad', module)
   .addDecorator(hostDecorator({}))
   .add('Open', () => (
@@ -15,4 +30,5 @@ storiesOf('Components|NumericKeypad.LateralKeypad', module)
       onChange={action('onChange')}
       onDeleteSelect={action('onDeleteSelect')}
     />
-  ));
+  ))
+  .add('Animation', () => <ShowAnimation />);
