02d1ee050bfbdc040260ae574d226dead986c335
GibboK <gibbok.coding@gmail.com>
2020-02-26 17:23:21 +0100

    Rename test file to .ts

---
+++ src/components/BatteryIcon/BatteryIcon.spec.ts
@@ -0,0 +1,48 @@
+import { getColorFromLevel, getColorFromVariant, getWidthFromLevel } from './utility';
+import { orange, lightGreen, red, common } from '../../colors';
+
+const COLOR_GREEN = lightGreen.A700;
+const COLOR_ORANGE = orange[500];
+
+const COLOR_RED = red[300];
+const COLOR_WHITE = common.white;
+const COLOR_BLACK = common.black;
+
+describe('BatteryIcon', () => {
+  describe('getColotFromLevel', () => {
+    it('should return green', () => {
+      expect(getColorFromLevel(100)).toBe(COLOR_GREEN);
+      expect(getColorFromLevel(80)).toBe(COLOR_GREEN);
+    });
+    it('should return orange', () => {
+      expect(getColorFromLevel(20)).toBe(COLOR_ORANGE);
+      expect(getColorFromLevel(15)).toBe(COLOR_ORANGE);
+    });
+    it('should return red', () => {
+      expect(getColorFromLevel(10)).toBe(COLOR_RED);
+      expect(getColorFromLevel(0)).toBe(COLOR_RED);
+      expect(getColorFromLevel(-10)).toBe(COLOR_RED);
+    });
+  });
+
+  describe('getWidthFromLevel', () => {
+    it('should return 100% width', () => {
+      expect(getWidthFromLevel(100)).toBe(41);
+    });
+    it('should return 50% width', () => {
+      expect(getWidthFromLevel(50)).toBe(21);
+    });
+    it('should return 0% width', () => {
+      expect(getWidthFromLevel(0)).toBe(0);
+    });
+  });
+
+  describe('getColorFromVariant', () => {
+    it('should return white', () => {
+      expect(getColorFromVariant('light')).toBe(COLOR_WHITE);
+    });
+    it('should return black', () => {
+      expect(getColorFromVariant('dark')).toBe(COLOR_BLACK);
+    });
+  });
+});
