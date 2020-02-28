af1ea5d6f3bc10ca72f4d146905797e321849a6e
GibboK <gibbok@users.noreply.github.com>
2020-02-28 15:00:31 +0100

    Fix issue with loading CSS (#82)
    Fix https://github.com/Actyx/actyx-ui/issues/81
    Convert CSS files to JSS so they do not need to be bundled in lib output folder.

--- src/components/VirtualizedList/VirtualizedList.tsx
+++ src/components/VirtualizedList/VirtualizedList.tsx
@@ -1,9 +1,64 @@
-import { getScrollbarWidth } from '../../utils';
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
+// import { getScrollbarWidth } from '../../utils';
 import * as classNames from 'classnames';
 import * as React from 'react';
 import Measure from 'react-measure';
 import { ListChildComponentProps, VariableSizeList } from 'react-window';
-import './virtualized-list.css';
+// FIXME our custom type for `react-jss` currently do not support `createUseStyles`
+//@ts-ignore
+import { createUseStyles } from 'react-jss';
+import { theme } from '../../theme';
+
+const useStyles = createUseStyles({
+  list: {
+    height: '100%',
+    backgroundColor: theme.palette.common.white,
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  listHeader: {
+    display: 'flex',
+    borderBottom: '1px solid #d6d6d6',
+    '& > div': {
+      padding: 10
+    },
+    '& > div:first-child': {
+      marginLeft: 14
+    },
+    '& > div:last-child': {
+      marginRight: 14
+    }
+    //FIXME add compensate scollbar
+  },
+  listBody: {
+    '& > div > div': {
+      cursor: 'pointer',
+      borderBottom: '1px solid #d6d6d6',
+      fontSize: 22,
+      height: 80,
+      display: 'flex',
+      alignItems: 'center'
+    },
+    '& > div > div > div': {
+      padding: 10
+    },
+    '& > div > div > div.numeric': {
+      textAlight: 'right'
+    },
+    '& > div > div > div.first-child': {
+      marginLeft: 14
+    },
+    '& > div > div > div.last-child': {
+      marginRight: 14
+    },
+    '& div > div.zebra': {
+      backgroundColor: '#fafafa;'
+    },
+    '& div > div.selected': {
+      backgroundColor: '#bee5fa;'
+    }
+  }
+});
 
 type Props<T> = Readonly<{
   className?: string;
@@ -35,6 +90,7 @@ export function VirtualizedList<T>({
     const datum = data[index];
 
     return (
+      //FIXME use for setRowClassName inline style
       <div
         key={getUniqKey(datum)}
         className={classNames(setRowClassName && setRowClassName(datum), {
@@ -51,20 +107,23 @@ export function VirtualizedList<T>({
 
   const [height, setHeight] = React.useState();
 
-  const headerElement = document.getElementsByClassName('ax-virtualized-list-header')[0];
-  if (headerElement) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    headerElement.style.setProperty('--ax-scrollbar', `${getScrollbarWidth() + 14}px`);
-  }
+  // FIXME add compensation scrollbar
+  // const headerElement = document.getElementsByClassName('ax-virtualized-list-header')[0];
+  // if (headerElement) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  // headerElement.style.setProperty('--ax-scrollbar', `${getScrollbarWidth() + 14}px`);
+  // }
+
+  const classes = useStyles();
 
   return (
     <Measure bounds onResize={({ bounds }) => bounds && setHeight(bounds.height)}>
       {({ measureRef }) => (
-        <div className={classNames('ax-virtualized-list', className)}>
+        <div className={classNames(classes.list, className)}>
           {header && (
             <div
-              className={classNames('ax-virtualized-list-header', {
+              className={classNames(classes.listHeader, {
                 compensate: height && data.length > height / 80
               })}
             >
@@ -74,7 +133,7 @@ export function VirtualizedList<T>({
           <div ref={measureRef} style={{ flex: 1 }}>
             {height && (
               <VariableSizeList
-                className="ax-virtualized-list-body"
+                className={classes.listBody}
                 width="100%"
                 height={height}
                 itemCount={data.length}
