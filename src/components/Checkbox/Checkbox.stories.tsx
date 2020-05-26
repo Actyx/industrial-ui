538adb3254345f585deeb303c1c55a458d11a617
GibboK <gibbok@users.noreply.github.com>
2020-05-26 07:22:24 -0700

    Fix bug touch ripple extend, update visual tests and snapshots (#101)

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,12 +1,12 @@
 /*
  * Copyright 2020 Actyx AG
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,6 @@
  */
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
-import { hostDecorator } from '../../utils';
 import * as React from 'react';
 import { CheckboxState, CheckboxColor, Checkbox } from './Checkbox';
 
@@ -29,7 +28,6 @@ const baseProps = {
 };
 
 storiesOf('Components/Checkbox', module)
-  .addDecorator(hostDecorator())
   .add('Indeterminate', () => <Checkbox {...baseProps} state={'indeterminate'} />)
   .add('Unchecked', () => <Checkbox {...baseProps} />)
   .add('Checked', () => <Checkbox {...baseProps} state={'checked'} />)
