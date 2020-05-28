3a8bc0784f538a5804fbe880c6cd280e4d59466c
GibboK <gibbok.coding@gmail.com>
2020-05-28 17:02:20 +0200

    load react-docgen-typescript-loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -16,6 +16,9 @@ module.exports = ({ config }) => {
       {
         loader: require.resolve("awesome-typescript-loader")
       },
+      {
+        loader: require.resolve('react-docgen-typescript-loader'),
+      },
     ]
   });
   config.resolve.extensions.push(".ts", ".tsx");
