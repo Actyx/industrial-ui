3d08b31e5eb0f4139d6fd8706ff4c834d56dae5c
GibboK <gibbok.coding@gmail.com>
2020-05-28 17:04:43 +0200

    remove test

--- src/components/Test/Test.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import { Test } from './Test';
-
-storiesOf('00_Test', module)
-  .addParameters({ component: Test })
-  .add('Test', () => <Test name="Hello world!" />);
