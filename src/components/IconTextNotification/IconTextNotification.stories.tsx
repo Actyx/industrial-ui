de6e45d311010b46ec4b0ff995f4adfb238cb241
GibboK <gibbok.coding@gmail.com>
2020-06-01 10:37:25 +0200

    inline baseprops for story

--- src/components/IconTextNotification/IconTextNotification.stories.tsx
+++ src/components/IconTextNotification/IconTextNotification.stories.tsx
@@ -20,23 +20,29 @@ import * as React from 'react';
 import { IconTextNotification } from './IconTextNotification';
 import { red } from '../../colors';
 
-const warningBaseProps = {
-  icon: 'warning',
-  text: 'Interrupted',
-  color: red.A700
-};
-
-const genericBaseProps = {
-  icon: 'info',
-  text: 'Generic text',
-  color: theme.palette.primary.main,
-  counter: 17
-};
-
 storiesOf('Components/IconTextNotification', module)
   .addParameters({ component: IconTextNotification })
   .addDecorator(hostDecorator({}))
-  .add('Warning', () => <IconTextNotification {...warningBaseProps} counter={3} />)
-  .add('Warning counter', () => <IconTextNotification {...warningBaseProps} />)
-  .add('Generic counter', () => <IconTextNotification {...genericBaseProps} />)
-  .add('Small size', () => <IconTextNotification {...genericBaseProps} smallSize />);
+  .add('Warning', () => (
+    <IconTextNotification icon="warning" text="Interrupted" color={red.A700} counter={3} />
+  ))
+  .add('Warning counter', () => (
+    <IconTextNotification icon="warning" text="Interrupted" color={red.A700} />
+  ))
+  .add('Generic counter', () => (
+    <IconTextNotification
+      icon="info"
+      text="Generic text"
+      color={theme.palette.primary.main}
+      counter={17}
+    />
+  ))
+  .add('Small size', () => (
+    <IconTextNotification
+      icon="info"
+      text="Generic text"
+      color={theme.palette.primary.main}
+      counter={17}
+      smallSize
+    />
+  ));
