de490ff307f23ed8c8ae9f9699cc86d206ab0f57
GibboK <gibbok.coding@gmail.com>
2020-06-01 09:38:38 +0200

    change story for better docs

--- src/components/ButtonStatus/ButtonStatus.stories.tsx
+++ src/components/ButtonStatus/ButtonStatus.stories.tsx
@@ -19,57 +19,22 @@ import { hostDecorator } from '../../utils';
 import * as React from 'react';
 import { ButtonStatus } from './ButtonStatus';
 
-const baseProps = {
-  text: 'Platform 1',
-  onSelect: action('onSelect')
-};
-
 storiesOf('Components/ButtonStatus', module)
   .addParameters({ component: ButtonStatus })
   .addDecorator(hostDecorator())
 
-  .add('Selected icon', () => {
-    const props = {
-      ...baseProps,
-      selected: true,
-      icon: 'person'
-    };
-    return <ButtonStatus {...props} />;
-  })
-  .add('Unselected icon', () => {
-    const props = {
-      ...baseProps,
-      selected: false,
-      icon: 'person'
-    };
-    return <ButtonStatus {...props} />;
-  })
-  .add('Selected no icon', () => {
-    const props = {
-      ...baseProps,
-      selected: true
-    };
-    return <ButtonStatus {...props} />;
-  })
-  .add('Unselected no icon', () => {
-    const props = {
-      ...baseProps,
-      selected: false
-    };
-    return <ButtonStatus {...props} />;
-  })
-  .add('Text', () => {
-    const props = {
-      ...baseProps,
-      selected: false
-    };
-    return <ButtonStatus {...props} />;
-  })
-  .add('Long text', () => {
-    const props = {
-      ...baseProps,
-      text: 'Very long content',
-      selected: false
-    };
-    return <ButtonStatus {...props} />;
-  });
+  .add('Selected icon', () => (
+    <ButtonStatus text="Platform 1" selected icon="person" onSelect={action('onSelect')} />
+  ))
+  .add('Unselected icon', () => (
+    <ButtonStatus text="Platform 1" selected={false} icon="person" onSelect={action('onSelect')} />
+  ))
+  .add('Selected no icon', () => (
+    <ButtonStatus text="Platform 1" selected={true} onSelect={action('onSelect')} />
+  ))
+  .add('Unselected no icon', () => (
+    <ButtonStatus text="Platform 1" selected={false} onSelect={action('onSelect')} />
+  ))
+  .add('Long text', () => (
+    <ButtonStatus text="Very long content" selected={false} onSelect={action('onSelect')} />
+  ));
