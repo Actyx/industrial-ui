0992eceb6bfa08230e9602a0f64ec4bb9d532abd
GibboK <gibbok.coding@gmail.com>
2020-06-01 17:16:57 +0200

    inline type and fix story for StatusCounter

--- src/components/StatusCounter/StatusCounters.stories.tsx
+++ src/components/StatusCounter/StatusCounters.stories.tsx
@@ -16,22 +16,30 @@
 import { storiesOf } from '@storybook/react';
 import { hostDecorator } from '../../utils';
 import * as React from 'react';
-import { StatusCounters, StatusCountersCounter } from './StatusCounters';
+import { StatusCounters } from './StatusCounters';
 import { StatusCounter } from './StatusCounter';
-
-const ready: StatusCountersCounter = { variant: 'ready', counter: 2 };
-const unknown: StatusCountersCounter = { variant: 'unknown', counter: 4 };
-const requested: StatusCountersCounter = { variant: 'requested', counter: 19 };
-const ex1 = [ready];
-const ex2 = [unknown, requested];
-const ex3 = [ready, unknown, requested];
 
 storiesOf('Components/StatusCounters', module)
   .addParameters({ component: StatusCounters })
   .addDecorator(hostDecorator())
-  .add('One counter', () => <StatusCounters counters={ex1} />)
-  .add('Two counters', () => <StatusCounters counters={ex2} />)
-  .add('Tree counters', () => <StatusCounters counters={ex3} />)
+  .add('One counter', () => <StatusCounters counters={[{ variant: 'ready', counter: 2 }]} />)
+  .add('Two counters', () => (
+    <StatusCounters
+      counters={[
+        { variant: 'unknown', counter: 4 },
+        { variant: 'requested', counter: 19 }
+      ]}
+    />
+  ))
+  .add('Tree counters', () => (
+    <StatusCounters
+      counters={[
+        { variant: 'ready', counter: 2 },
+        { variant: 'unknown', counter: 4 },
+        { variant: 'requested', counter: 19 }
+      ]}
+    />
+  ))
   .add('Part ready', () => <StatusCounter counter={1} variant="ready" />)
   .add('Part requested', () => <StatusCounter counter={22} variant="requested" />)
   .add('Part unknown', () => <StatusCounter counter={764} variant="unknown" />)
