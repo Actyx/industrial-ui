7050a77ce764775d59d6d50416e9d3e7a8edc9c8
GibboK <gibbok@users.noreply.github.com>
2020-06-25 12:52:35 +0200

    Revise Lorem (#146)
    Revise Lorem Ipsum for Storybook and update visual regressions tests references.
    Fix: https://github.com/Actyx/industrial-ui/issues/117

--- src/utils/lorem.ts
+++ src/utils/lorem.ts
@@ -13,9 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const LoremIpsumShort =
+
+export const LoremSM = 'Lorem ipsum dolor sit amet.';
+
+export const LoremMD =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.';
 
-export const LoremIpsumVeryShort = 'Lorem ipsum dolor sit amet.';
-
-export const LoremIpsum = `${LoremIpsumShort} Ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Sed faucibus turpis in eu mi bibendum neque egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Pharetra sit amet aliquam id diam maecenas ultricies mi. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Risus nec feugiat in fermentum posuere urna nec tincidunt praesent. Quis lectus nulla at volutpat diam. At consectetur lorem donec massa sapien faucibus. Aliquet nibh praesent tristique magna sit amet.\n`;
+export const LoremXL = `${LoremMD.repeat(5)}`;
